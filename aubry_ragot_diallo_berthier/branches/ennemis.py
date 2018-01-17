0c435e3a1947660b8a2e6e7e31baa925b9e5e572
theoberthier <35524169+theoberthier@users.noreply.github.com>
2018-01-17 11:26:54 +0100

    hero et ennemis en objet
    transposition du début de code hero/ennemis orienté objet

---
+++ aubry_ragot_diallo_berthier/branches/ennemis.py
@@ -0,0 +1,13 @@
+class ennemis:
+
+    def __init__(self,grid):
+
+        self.grid = grid
+
+    def ajoutEnnemis(grid):
+        for i in range(2):
+            for j in range(l):
+                grid[i][j] = ennemis
+        return self.grid
+
+# ajout des déplacement des ennemis
---
+++ aubry_ragot_diallo_berthier/branches/hero.py
@@ -0,0 +1,17 @@
+class hero:
+
+    def __init__(self,grid):
+
+        self.grid = grid
+
+    def ajoutHero(grid): # Permet de mettre le joueur a sa place dans la grille.
+        hauteurMap = h - 1
+        centre = math.ceil(l/2) - 1 #placement du hero
+        for i in range(h):
+            for j in range(l):
+                self.grid[hauteurMap][centre] = hero # ajout du hero dans le centre de la liste
+
+#ajout déplacement
+
+monhero = hero(grid)
+monhero.ajoutHero(grid)
