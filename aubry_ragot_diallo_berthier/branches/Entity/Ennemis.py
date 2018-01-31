ea87d654a7457ba6d05997af1552bb1652311b1f
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-01-31 11:25:42 +0100

    Ajout ennemis.py
    Ajout de documentation et renommage de variable.

--- aubry_ragot_diallo_berthier/branches/Entity/Ennemis.py
+++ aubry_ragot_diallo_berthier/branches/Entity/Ennemis.py
@@ -1,46 +1,43 @@
 import math
 import random as rd
 
-def rd_POS(y):
-    return rd.randint(0,1),rd.randint(0,(y - 1))
 
 
-class ennemis:
 
-    def __init__(self):
-        self.posX = 0
-        self.posY = 0
+class ennemis:#Class qui définit un ennemi.
 
-    def ajoutEnnemis(self, lEnnemis, maGrille): #Permet l'ajout d'un ennemis de façon aléatoire.
+    def __init__(self):#Constructeur de la class.
+        self.posX = 0#Position X de départ.
+        self.posY = 0#Position Y de départ.
+
+    def rd_POS(self,colonneGrille):#Ajoute une position random à un ennemis.
+        return rd.randint(0,1),rd.randint(0,(colonneGrille - 1))#Utilise la fonction randint permettant de mettre aléatoire.
+
+    def ajoutEnnemis(self, listeEnnemis, maGrille): #Permet l'ajout d'un ennemis de façon aléatoire.
         #A ajouter
-        c_enn = "0"
+        caractereEnnemis = "0"# Le caractère d'un ennemi.
 
-        if len(lEnnemis) >= 1:
+        if len(listeEnnemis) >= 1:
             lPosEnnemis = []
-            for enn in lEnnemis:
+            for enn in listeEnnemis:
                 lPosEnnemis.append([enn.posX, enn.posY])
 
-            posX, posY = rd_POS(maGrille.colonne)  #Initialisation de position potentiel d'un nouvel ennemi
+            posX, posY = self.rd_POS(maGrille.colonne)  #Initialisation de position potentiel d'un nouvel ennemi.
 
             while [posX,posY] in lPosEnnemis:   #Vérification s'il en existe pas un de present sur les positions générés
-                posX, posY = rd_POS(maGrille.colonne)
+                posX, posY = self.rd_POS(maGrille.colonne)
         else:
-            posX, posY = rd_POS(maGrille.colonne)
+            posX, posY = self.rd_POS(maGrille.colonne)
 
         print(posX,posY)
         print(maGrille.grid[posX][posY])
 
-        self.posX = posX
-        self.posY = posY        
+        self.posX = posX#Définit la position de l'ennemi en X par rapport à la variable posX.
+        self.posY = posY#Définit la position de l'ennemi en Y par rapport à la variable posY.
 
-        maGrille.grid[self.posX][self.posY] = c_enn #Ajoute un ennemis de façon aléatoire dans le haut de la grille.
+        maGrille.grid[self.posX][self.posY] = caractereEnnemis #Ajoute un ennemis de façon aléatoire dans le haut de la grille.
 
-    def defile(self,maGrille):
+    def defile(self,maGrille):#Fait défiler un ennemi vers le bas.
+    #A COMPLETER
         if self.posX  == maGrille.colonne:#Vérifie que l'ennemis ne dépasse pas la hauteur.
             maGrille.grid[self.posX][self.posY] = "-"#Si c'est le cas alors on le suprime.
-
-
-
-    def collision(self):
-
-        return None
