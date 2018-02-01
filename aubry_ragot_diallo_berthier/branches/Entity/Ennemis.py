bd98600836c540f6a2a291ac976b3b897e63225d
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-01 14:06:09 +0100

    ajout doc

--- aubry_ragot_diallo_berthier/branches/Entity/Ennemis.py
+++ aubry_ragot_diallo_berthier/branches/Entity/Ennemis.py
@@ -16,22 +16,21 @@ class ennemis:#Class qui définit un ennemi.
     def ajoutEnnemis(self, dictEnnemis, maGrille): #Permet l'ajout d'un ennemis de façon aléatoire.
         #A ajouter
         caractereEnnemis = "0"# Le caractère d'un ennemi.
-        print(dictEnnemis)
-        if len(dictEnnemis) >= 1:
-            lPosEnnemis = []
-            for enn in dictEnnemis.values():
-                print(enn)
-                lPosEnnemis.append([enn.posX, enn.posY])
+        print(dictEnnemis)#Affiche le dictionnaire des ennemis.
+        if len(dictEnnemis) >= 1:#Si le dictionnaire d'ennemis est supérieur ou égal à 1 alors.
+            lPosEnnemis = []#Liste des positions des ennemis.
+            for ennemi in dictEnnemis.values():#Boucle qui parcourt le dictionnaire des ennemis.
+                lPosEnnemis.append([ennemi.posX, ennemi.posY])#Ajoute dans la liste des position des ennemis, chaque ennemis.
 
             posX, posY = self.rd_POS(maGrille.colonne)  #Initialisation de position potentiel d'un nouvel ennemi.
 
-            while [posX,posY] in lPosEnnemis:   #Vérification s'il en existe pas un de present sur les positions générés
-                posX, posY = self.rd_POS(maGrille.colonne)
-        else:
+            while [posX,posY] in lPosEnnemis:   #Tant que la posX et posY sont dans la liste des positions ennemis.
+                posX, posY = self.rd_POS(maGrille.colonne)#Les positions X et Y sont égales à la position aléatoire d'un ennemis généré.
+        else:#Sinon
             posX, posY = self.rd_POS(maGrille.colonne)
 
-        print(posX,posY)
-        print(maGrille.grid[posX][posY])
+        print(posX,posY)#Affiche la position de l'ennemi.
+        print(maGrille.grid[posX][posY])#Affiche sur la grille la position de l'ennemi.
 
         self.posX = posX#Définit la position de l'ennemi en X par rapport à la variable posX.
         self.posY = posY#Définit la position de l'ennemi en Y par rapport à la variable posY.
