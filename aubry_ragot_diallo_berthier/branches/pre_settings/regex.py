0c61b7ce184ed3c4c750fc6fdcf402213ea351ec
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-01 15:17:19 +0100

    Add files via upload

--- aubry_ragot_diallo_berthier/branches/pre_settings/regex.py
+++ aubry_ragot_diallo_berthier/branches/pre_settings/regex.py
@@ -1,10 +1,12 @@
-import re
-
-
-re.findall([-{2,3}], line)
-    for line in open()
-
 
 
  #permet de regarder pour la variable.
 #[_{2,3}] #permet de regarder pour la variable.
+import re
+with open('settings.txt') as f:
+    for line in f:
+        match = re.search('^-.*-', line)
+        test = re.search('_.*_', line)
+
+print(match)
+print(test)
