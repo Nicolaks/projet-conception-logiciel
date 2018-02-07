9ce2820c90709ecb633bfa18451b2a53c21bc1c2
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-07 12:03:48 +0100

    0.0.9.5
    Ajout de documention pour la fonction grile.

--- aubry_ragot_diallo_berthier/branches/Grille.py
+++ aubry_ragot_diallo_berthier/branches/Grille.py
@@ -5,10 +5,9 @@ class grille:
 
     def __init__(self, ligne, colonne):
         #Constructeur
-        self.ligne = ligne
-        self.colonne = colonne
-        self.grid = []
-
+        self.ligne = ligne#La ligne de notre grille.
+        self.colonne = colonne#La colonne de notre grille.
+        self.grid = []#Permet de créer grille
     def creerGrille(self, val):
         #grid = []
         for i in range(self.ligne):#On parcours le nombres de ligne
@@ -18,16 +17,15 @@ class grille:
         return self.grid#Retourne la grille.
 
 
-    def affiche(self):
+    def affiche(self):#Fonction qui affiche la grille.
         os.system("clear")#Vide la console a chaque affichage
         #Affiche en mode console
         ch = ""#Créer une liste vide.
 
-        for liste in self.grid:
-            for c in liste:
+        for liste in self.grid:#Boucle qui recherche dans la grille.
+            for c in liste:#Boucle qui recherche dans la liste.
                 ch += str(c + 5*" ")
             ch += "\n\n"
-        print(ch)
+        print(ch)#Affiche la grille.
 
 #On peut gerer diféramment la grille où l'on ne référence que les objets TIRS,Ennemies,HERO et où l'on ne référence AUCUNE case vide.
-
