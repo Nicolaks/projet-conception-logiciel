5402a5037a722c1f6958a00cdc446f7595506f3a
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-07 11:29:30 +0100

    0.0.9.5
    Ajout de documentation.

--- aubry_ragot_diallo_berthier/branches/pre_settings/Settings.py
+++ aubry_ragot_diallo_berthier/branches/pre_settings/Settings.py
@@ -1,28 +1,27 @@
-#Fichier de sauvegarde de SCORE, changement des touches assignés, récupération des scores et touches
 
-try:
-    fichier = open("settings.txt","r")
-except:
-    fichier = open("settings.txt","w")#on creer et ecrit dans settings.txt les differentes touche du jeux
-    fichier.write("up=pygame.KUP\n")
-    fichier.write("down=pygame.KDOWN\n")
-    fichier.write("left=pygame.KLEFT\n")
-    fichier.write("right=pygame.KRIGHT\n"),
-    fichier.write("tir=pygame.KSPACE\n")
-    fichier.write("scores={}\n")
-fichier.close()
-
-
-def lecture():
-
-    fichier = open("settings.txt", "r")
-    tableau = {}
-    for ligne in fichier:
-        ligne = ligne.replace("\n", "")
-        ligne = ligne.split("=")
-        tableau[ligne[0]] = ligne[1]
-
-    print(tableau)
+try:#On regarde si le fichier existe ou non.
+    fichier = open("settings.txt","r")#Si il existe on le regarde à l'intérieur.
+except:#Sinon.
+    fichier = open("settings.txt","w")#on creer et ecrit dans settings.txt les differentes touches du jeux
+    fichier.write("up=pygame.KUP\n")#Mets les valeurs.
+    fichier.write("down=pygame.KDOWN\n")#Mets les valeurs.
+    fichier.write("left=pygame.KLEFT\n")#Mets les valeurs.
+    fichier.write("right=pygame.KRIGHT\n"),#Mets les valeurs.
+    fichier.write("tir=pygame.KSPACE\n")#Mets les valeurs.
+    fichier.write("scores={}\n")#Mets les valeurs.
+fichier.close()#Ferme le fichier
 
 
-lecture()
+def lecture():#Fonction qui va faire une lecture du fichier pour en extraire des informations.
+
+    fichier = open("settings.txt", "r")#Ouvre le fichier.
+    tableau = {}#Initialise un dictionnaire vide.
+    for ligne in fichier:#On regarde chaque ligne du fichier.
+        ligne = ligne.replace("\n", "")#On enlève les retours à la ligne.
+        ligne = ligne.split("=")#On split par rapport au signe "=".
+        tableau[ligne[0]] = ligne[1]#On ajoute dans le dictionnaire les nouvelles valeurs.
+
+    print(tableau)#On affiche pour le vérifier.
+
+
+lecture()#On lance la fonction.
