d350035e87ed4f668b6a573887b0bf1f76c10340
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-14 11:50:51 +0100

    Add files via upload

--- aubry_ragot_diallo_berthier/branches/interface_tkinter.py
+++ aubry_ragot_diallo_berthier/branches/interface_tkinter.py
@@ -1,43 +1,42 @@
 from tkinter import *
-from PIL import Image, ImageTK
-
-
+#from PIL import Image, ImageTK
 
 
 class App():#Class de type App qui gère la fenêtre graphique de base.
     def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
+        self.frame = Frame(master)
+        self.frame.pack()
 
-        self.jouer_button = Button(frame, text="Jouer", fg="#ff1122",
+        self.jouer_button = Button(self.frame, text="Jouer", fg="#ff1122",#
         relief=RAISED, compound=CENTER,
         width=25, height=5,
-        command=self.lancerPartie).pack()#Ajout d'un boutton pour lancer le jeux.
+        command=self.lancerPartie).pack()#Ajout d'un boutton pour lancer le jeux et quitter la fenêtre.
 
-        self.settings_button = Button(frame, text="Paramètres", fg="#aa5500",
+        self.settings_button = Button(self.frame, text="Paramètres", fg="#aa5500",
         relief=RAISED, compound=CENTER,
         width=25, height=5,
         command=self.settings).pack()#Ajout d'un boutton pour l'accès aux paramètres.
 
-        self.quitter_button = Button(frame, text="Quitter", fg="#e84118",
+        self.quitter_button = Button(self.frame, text="Quitter", fg="#e84118",
         relief=RAISED, compound=CENTER,
         width=25, height=5,
-        command=frame.quit).pack()#Ajout d'un boutton pour quitter la fenêtre.
+        command=self.frame.quit).pack()#Ajout d'un boutton pour quitter la fenêtre.
 
 
 
 
-    def lancerPartie(self):
+    def lancerPartie(self):#Fonction lancerPartie qui va lancer la partie puis quitter la fenêtre.
         print ("Lancement de la partie")
-
+        self.frame.quit()
     def settings(self):
         print("Accès aux paramètres")
 
 root = Tk()
 root.title("Le jeu trop bien !!!")
 root.geometry("500x400")#Fenètre de 500 par 400
-image = Image.open("terre.jpg")
-photo = ImageTK.PhotoImage(image)
+
+#Mettre en fond d'écran du programme cette image, mais ne fonctionne pas.
+#image = Image.open("terre.jpg")#censer ouvrir l'image.
+#photo = ImageTK.PhotoImage(image)#cencer appliquer l'image.
 app = App(root)
 root.mainloop()#Boucle principal
-print(root.grid_size())
