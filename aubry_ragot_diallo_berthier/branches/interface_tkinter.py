adc12c8ade1c0f7d8d5ec01b3288d77bdee4d766
Nicolaks <32768134+Nicolaks@users.noreply.github.com>
2018-02-14 11:32:28 +0100

    Ajout de l'interface tkinter

--- aubry_ragot_diallo_berthier/branches/interface_tkinter.py
+++ aubry_ragot_diallo_berthier/branches/interface_tkinter.py
@@ -1,21 +1,43 @@
 from tkinter import *
+from PIL import Image, ImageTK
 
 
-class App():
+
+
+class App():#Class de type App qui gère la fenêtre graphique de base.
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        self.fenetre = create_window()
-        self.qbutton = Button(frame, text="Quitter", fg="#e84118",
-        command=frame.quit)
-        self.qbutton.pack(side=LEFT)
-        self.pbutton = Button(frame, text="Jouer", command=self.lancerPartie)
-        self.pbutton.pack(side=LEFT)
+
+        self.jouer_button = Button(frame, text="Jouer", fg="#ff1122",
+        relief=RAISED, compound=CENTER,
+        width=25, height=5,
+        command=self.lancerPartie).pack()#Ajout d'un boutton pour lancer le jeux.
+
+        self.settings_button = Button(frame, text="Paramètres", fg="#aa5500",
+        relief=RAISED, compound=CENTER,
+        width=25, height=5,
+        command=self.settings).pack()#Ajout d'un boutton pour l'accès aux paramètres.
+
+        self.quitter_button = Button(frame, text="Quitter", fg="#e84118",
+        relief=RAISED, compound=CENTER,
+        width=25, height=5,
+        command=frame.quit).pack()#Ajout d'un boutton pour quitter la fenêtre.
+
+
+
 
     def lancerPartie(self):
         print ("Lancement de la partie")
 
+    def settings(self):
+        print("Accès aux paramètres")
 
 root = Tk()
+root.title("Le jeu trop bien !!!")
+root.geometry("500x400")#Fenètre de 500 par 400
+image = Image.open("terre.jpg")
+photo = ImageTK.PhotoImage(image)
 app = App(root)
-root.mainloop()
+root.mainloop()#Boucle principal
+print(root.grid_size())
