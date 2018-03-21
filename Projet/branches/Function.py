5ce8defd00d3f95519c8a0d03d17a1b1691c668a
Kyllzery <35502263+Kyllzery@users.noreply.github.com>
2018-03-21 17:24:40 +0100

    Delete Function.py

--- Projet/branches/Function.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-import pygame
-from pygame.locals import *
-
-def __Draw():
-    Window.blit(Background, (0,0))
-    Window.blit(Spaceship.style, (Spaceship.posX, Spaceship.posY))
