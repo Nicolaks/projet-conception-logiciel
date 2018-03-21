f6b2c86c24156f7616c8502f1b084d11627d9000
theoberthier <35524169+theoberthier@users.noreply.github.com>
2018-03-21 17:21:18 +0100

    Add files via upload

--- Projet/branches/scroling.py
+++ Projet/branches/scroling.py
@@ -1,17 +1,77 @@
-try:
-    import os
-    import sys
-    import time
-    import pygame
-    import json
-    from pygame.locals import *
+import os
+import sys
+import time
+import pygame
+import json
+from pygame.locals import *
 
-    import pre_settings.Settings as Settings
-    import Entity.Reactor as Rct
-    import Entity.EntityGroup as ENTGroup
-    import Entity.SpaceShip as spaceShip
-    import Entity.Ally as Ally
-    import Entity.Bullet as Blt
-except ImportError as error:
-    print(error.__class__.__name__ + " : " + error.msg)
-    sys.exit(0)
+class Background():
+    def __init__(self,bckg_dict, Width,Height):
+        self.dict = bckg_dict
+        self.image = None
+        self.Width, self.Height = Width,Height
+        self.y = 0
+        self.pos_y = 0
+
+        self.change(str("Background"))
+        self.Pause = False
+
+    def change(self,name):
+        self.image = pygame.image.load(os.path.join("..", "Ressources", "Background",name+".jpg")).convert()
+        self.image = pygame.transform.scale(self.image, (self.Width,self.Height))#Charge l'image
+
+    def draw(self, Window):
+        if not self.Pause:
+            Window.blit(self.image,(0,self.pos_y - self.Height))
+            self.y -= 1
+            self.pos_y = self.y % self.Height
+            if self.pos_y <= self.Height:
+                Window.blit(self.image,(0,self.pos_y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+pygame.init()
+Width = 800
+Height = 600
+fps = 60
+BLACK = (255,255,255)
+Window = pygame.display.set_mode((Width, Height))
+Background = pygame.image.load(os.path.join("..", "Ressources", "Background","Background.jpg")).convert()
+Background = pygame.transform.scale(Background, (Width,Height))#Charge l'image
+y = 0
+Clock = pygame.time.Clock()
+pygame.display.set_caption("Manic Shooter : Shot'em up !")
+condition = True
+while condition:
+    delta_time = Clock.tick(fps) * 0.001
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:#Si on ferme la fenêtre en cliquant sur la CROIX
+            condition = False
+    Window.fill(BLACK)
+    pos_y = y % Background.get_rect().height
+    Window.blit(Background,(0,pos_y - Background.get_rect().height))#difference de pos
+    y -= 1 #vitesse scroling
+    if pos_y < Height:
+        Window.blit(Background,(0,pos_y))
+
+    pygame.display.update()
+
+
+pygame.quit()
+quit()
+"""
